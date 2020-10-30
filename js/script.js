1a11f58c36b77edaedb739c2681405e28a0940d2
lebauerle <63855379+lebauerle@users.noreply.github.com>
2020-10-30 17:38:12 +0100

    File structure + 5 quotes within objects

---
+++ js/script.js
@@ -0,0 +1,73 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+const quotes = [
+  {
+    //1st quote Darwin
+    quote:"It is not the strongest of the species that survive, nor the most intelligent, but the one most responsive to change." ,
+    source:"Charles Darwin",
+    citation:"Origin of the Species",
+    year: 1859,
+    tags: "Science"
+  },
+  {
+    quote:"Niemand hat die Absicht, eine Mauer zu errichten!" ,
+    source:"Walter Ulbricht",
+    citation:"press conference",
+    year: 1961,
+    tags: "Politics"
+  },
+  {
+    quote:"It is better to offer no excuse than a bad one" ,
+    source:"George Washington",
+    citation:"Letter to his niece",
+    year: 1791,
+    tags: "History"
+  },
+  {
+    quote:"Your time is limited, so don't waste it living someone else's life." ,
+    source:"Steve Jobs",
+    citation:"'Steve Jobs talks about the Library of Congress'",
+    year: 1990,
+    tags: "Motivation"
+  },
+  {
+    quote:"So I express the Republicans are minority, so I have some sympathy towards them." ,
+    source:"The Dalai Lama",
+    citation:"Compassionate Ethics in Difficult Times",
+    year: 2010,
+    tags: "Ethics"
+  }
+]
+;
+
+/***
+ * `getRandomQuote` function
+***/
+
+//random number
+//return object with random number
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
