587292d158e381a40caaedc282b538c718754f39
lebauerle <63855379+lebauerle@users.noreply.github.com>
2020-11-02 11:24:32 +0100

    updated quotes, functions+timer are working,

--- js/script.js
+++ js/script.js
@@ -10,9 +10,9 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+// created 5 'full'-quote objects and 3 with missing elements
 const quotes = [
   {
-    //1st quote Darwin
     quote:"It is not the strongest of the species that survive, nor the most intelligent, but the one most responsive to change." ,
     source:"Charles Darwin",
     citation:"Origin of the Species",
@@ -24,7 +24,7 @@ const quotes = [
     source:"Walter Ulbricht",
     citation:"press conference",
     year: 1961,
-    tags: "Politics"
+    tags: "Politics, History"
   },
   {
     quote:"It is better to offer no excuse than a bad one" ,
@@ -38,14 +38,28 @@ const quotes = [
     source:"Steve Jobs",
     citation:"'Steve Jobs talks about the Library of Congress'",
     year: 1990,
-    tags: "Motivation"
+    tags: "Motivation, Focus"
   },
   {
     quote:"So I express the Republicans are minority, so I have some sympathy towards them." ,
     source:"The Dalai Lama",
     citation:"Compassionate Ethics in Difficult Times",
     year: 2010,
-    tags: "Ethics"
+    tags: "Ethics, Religion"
+  },
+  {
+    quote: "Be yourself; everyone else is already taken." ,
+    source:"Oscar Wilde "
+  },
+  {
+    quote:"Be the change that you wish to see in the world." ,
+    source:"Mahatma Gandhi",
+    tags: "Motivation, Inspiration"
+  },
+  {
+    quote:"If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals." ,
+    source:"J.K. Rowling",
+    citation:"Harry Potter and the Goblet of Fire"
   }
 ]
 ;
@@ -56,13 +70,52 @@ const quotes = [
 
 //random number
 //return object with random number
-
-
+//building a function thats returns a random quote
+function getRandomQuote () {
+//random number(from 1 to 8, because 8 objects) is stored in variable randomNumber
+ let randomNumber = Math.floor(Math.random() * 8);
+ //variable finalRQ stores the random quote
+ let finalRQ = quotes[randomNumber];
+ //the random quote is returned
+ return finalRQ
+};
 
 /***
  * `printQuote` function
 ***/
 
+//creating a function that displays the object
+function printQuote (){
+//storing the function (that creats a random quote) in variable rQuote
+const rQuote = getRandomQuote();
+//creating a variable that stores a template literal,with placeholder that creates the quote and source paragraph
+let displayHTMLItems =` <p class ="quote">${rQuote.quote}</p>
+              <p class ="source">${rQuote.source}`;
+//if the quote contains a citation, it will create a span
+if (rQuote.citation) {
+  displayHTMLItems +=`<span class="citation">${rQuote.citation}</span>`
+}
+//if the quote contains a year, it will create a span
+
+if (rQuote.year) {
+  displayHTMLItems +=`<span class="year">${rQuote.year}</span>`
+}
+//if the quote contains tags, it will create a span
+
+if (rQuote.tags) {
+  displayHTMLItems +=`<span class="tags"></br>${rQuote.tags}</span>`
+}; // if loop ends
+// create a variable that acceses and stores what is in the ID="quote-box"
+let quoteDiv = document.getElementById("quote-box");
+// set innerHTML property of quoteDiv to the string literal above 
+quoteDiv.innerHTML = displayHTMLItems;
+}  
+
+function refresher(){
+  printQuote();
+};
+
+setInterval(refresher, 5000);
 
 
 /***
